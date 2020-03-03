7e9d211209743cb9a2aeede77fc3a060fd278c81
asliergin <asliergin9@gmail.com>
2020-03-03 10:07:20 +0100

    update in unet for 3channel

--- main1d.py
+++ main1d.py
@@ -1,5 +1,6 @@
 from unet_1channel.model import *
 from unet_1channel.data import *
+import os
 
 #os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
@@ -11,12 +12,15 @@ data_gen_args = dict(rotation_range=0.2,
                     zoom_range=0.05,
                     horizontal_flip=True,
                     fill_mode='nearest')
-myGene = trainGenerator(2,'data1d/train','frames','masks',data_gen_args,save_to_dir = None)
+myGene = trainGenerator(2,'unet_1channel/data1d/train','frames','masks',data_gen_args,save_to_dir = None)
 
 model = unet()
 model_checkpoint = ModelCheckpoint('unet1d.hdf5', monitor='loss',verbose=1, save_best_only=True)
 model.fit_generator(myGene,steps_per_epoch=300,epochs=1,callbacks=[model_checkpoint])
 
-testGene = testGenerator("data1d/test")
+testGene = testGenerator("unet_1channel/data1d/test")
 results = model.predict_generator(testGene,30,verbose=1)
+
+if not os.path.exists("unet_1channel/data1d/results"):
+    os.makedirs("unet_1channel/data1d/results")
 saveResult("data1d/test",results)
--- main3d.py
+++ main3d.py
@@ -1,5 +1,7 @@
 from unet_3channel.model import *
 from unet_3channel.data import *
+import os
+
 
 #os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
@@ -11,12 +13,20 @@ data_gen_args = dict(rotation_range=0.2,
                     zoom_range=0.05,
                     horizontal_flip=True,
                     fill_mode='nearest')
-myGene = trainGenerator(2,'data3d/train','frames','masks',data_gen_args,save_to_dir = None)
+myGene = trainGenerator(2,'unet_3channel/data3d/train','frames','masks',data_gen_args,save_to_dir = None)
 
 model = unet()
 model_checkpoint = ModelCheckpoint('unet3d.hdf5', monitor='loss',verbose=1, save_best_only=True)
 model.fit_generator(myGene,steps_per_epoch=300,epochs=1,callbacks=[model_checkpoint])
 
-testGene = testGenerator("data3d/test")
-results = model.predict_generator(testGene,30,verbose=1)
-saveResult("data3d/test",results)
+
+
+testGene = testGenerator("unet_3channel/data3d/test")
+model = unet()
+model.load_weights("unet3d.hdf5")
+results = model.predict_generator(testGene, 10, verbose=1)
+
+if not os.path.exists("unet_3channel/data3d/results"):
+    os.makedirs("unet_3channel/data3d/results")
+
+saveResult("unet_3channel/data3d/results", results)
