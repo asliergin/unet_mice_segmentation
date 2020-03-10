338844bd3f325e70c49d233cbccde90eedc010e9
asliergin <asliergin9@gmail.com>
2020-03-10 16:27:47 +0100

    changes to visualize loss plot

--- main1d.py
+++ main1d.py
@@ -1,6 +1,7 @@
 from unet_1channel.model import *
 from unet_1channel.data import *
 import os
+import matplotlib.pyplot as plt
 
 #os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
@@ -12,11 +13,14 @@ data_gen_args = dict(rotation_range=0.2,
                     zoom_range=0.05,
                     horizontal_flip=True,
                     fill_mode='nearest')
+
+
 myGene = trainGenerator(2,'unet_1channel/data1d/train','frames','masks',data_gen_args,save_to_dir = None)
 
 model = unet()
 model_checkpoint = ModelCheckpoint('unet1d.hdf5', monitor='loss',verbose=1, save_best_only=True)
 model.fit_generator(myGene,steps_per_epoch=300,epochs=1,callbacks=[model_checkpoint])
+# try tensorboard instead
 
 testGene = testGenerator("unet_1channel/data1d/test")
 results = model.predict_generator(testGene,30,verbose=1)
@@ -24,3 +28,6 @@ results = model.predict_generator(testGene,30,verbose=1)
 if not os.path.exists("unet_1channel/data1d/results"):
     os.makedirs("unet_1channel/data1d/results")
 saveResult("data1d/test",results)
+
+
+
