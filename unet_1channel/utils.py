6b079d3a0608aa92db20c4387d81d1727a307830
asliergin <asliergin9@gmail.com>
2020-03-13 09:55:56 +0100

    training loss curves included

---
+++ unet_1channel/utils.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+
+
+#def plot_segm_history(history, metrics=["accuracy"], losses=["binary_crossentropy"]):
+def plot_segm_history(history):
+    """[summary]
+    Args:
+        history ([type]): [description]
+        metrics (list, optional): [description]. Defaults to ["iou", "val_iou"].
+        losses (list, optional): [description]. Defaults to ["loss", "val_loss"].
+    """
+    # summarize history for iou
+    plt.plot(history.history['accuracy'])
+#    plt.plot(history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+#    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+    # summarize history for loss
+    plt.plot(history.history['loss'])
+#    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+#    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
